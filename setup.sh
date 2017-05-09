2787b3e1ee8aba11bde6746770a2e1af8c105283
Bart Kessels <development@bk-mail.com>
2017-05-09 22:44:29 +0200

    Add albert launcher

--- setup.sh
+++ setup.sh
@@ -69,8 +69,10 @@ chmod +x fedy-installer
 rm fedy-installer
 
 # Enable copr repos
-dnf copr -y enable heikoada/gtk-themes
 dnf copr -y enable abn/ghostwriter
+dnf copr -y enable bartkessels/GetIt
+dnf copr -y enable heikoada/gtk-themes
+dnf copr -y enable rabiny/albert
 
 # Update
 dnf update -y
@@ -145,7 +147,7 @@ sed -i 's|clean_requirements_on_remove=True|clean_requirements_on_remove=False|g
 dnf install -y gnome-shell-extension-pomodoro gnome-shell-extension-drive-menu gnome-shell-extension-alternate-tab gnome-shell-extension-launch-new-instance gnome-shell-extension-topicons-plus
 
 # Utilities
-dnf install -y whois pandoc deja-dup grsync gpick pdfmod gnome-todo luckybackup ghostwriter ffmpeg ctags
+dnf install -y whois pandoc deja-dup grsync gpick pdfmod gnome-todo luckybackup ghostwriter ffmpeg ctags getit albert
 
 # Nautilus extension
 dnf install -y seahorse-nautilus gnome-terminal-nautilus
