ef7845e4bd8a7ef32e8c179ef0f3b3a983f1005f
Bart Kessels <bartkessels@bk-mail.com>
2017-12-14 23:20:07 +0100

    Fix small mistakes

--- GNOME/setup.sh
+++ GNOME/setup.sh
@@ -161,10 +161,10 @@ dnf install -y blender pitivi
 dnf install -y vim gnome-builder glade code @development-tools @gnome-software-development
 dnf install -y vim-nerdtree
 dnf install -y java-1.8.0-openjdk-devel automake cmake autoconf zlib-devel.i686 ncurses-devel.i686 ant gettext-devel autoconf-archive intltool itstool gtksourceview3-devel
-dnf install -y python3-jedi clang clang-libs nuget glide make
-dnf install -y dia meld sqlitebrowser pencil planner gitg
+dnf install -y python3-jedi clang clang-libs glide make
+dnf install -y dia meld sqlitebrowser gitg
 dnf install -y gcc
-dnf install -y gtk+-devel gtk3-devel libsoup-devel zlib.i686 ncurses-libs.i686 bzip2-libs.i686 python-devel python3-devel gtksourceview3-devel @development-libs
+dnf install -y gtk+-devel gtk3-devel libsoup-devel zlib.i686 ncurses-libs.i686 bzip2-libs.i686 python3-devel gtksourceview3-devel @development-libs
 dnf install -y fedora-packager fedora-review @rpm-development-tools meson ninja-build
 dnf install -y gtk3-devel-docs
 
